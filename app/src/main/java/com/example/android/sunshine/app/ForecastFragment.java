9dd0e658615cc599ae8fc37e000e4b4899d2eda9
Bojan <Bojan>
2015-10-10 21:05:50 +0200

    Parsiranje mog API-a II

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -23,7 +23,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -93,30 +92,8 @@ public class ForecastFragment extends Fragment {
 
         private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
 
-        /* The date/time conversion code is going to be moved outside the asynctask later,
-     * so for convenience we're breaking it out into its own method now.
-     */
-        private String getReadableDateString(long time){
-            // Because the API returns a unix timestamp (measured in seconds),
-            // it must be converted to milliseconds in order to be converted to valid date.
-            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-            return shortenedDateFormat.format(time);
-        }
-
         /**
-         * Prepare the weather high/lows for presentation.
-         */
-        private String formatHighLows(double high, double low) {
-            // For presentation, assume the user doesn't care about tenths of a degree.
-            long roundedHigh = Math.round(high);
-            long roundedLow = Math.round(low);
-
-            String highLowStr = roundedHigh + "/" + roundedLow;
-            return highLowStr;
-        }
-
-        /**
-         * Take the String representing the complete forecast in JSON Format and
+         * Take the String representing the complete ispis in JSON Format and
          * pull out the data we need to construct the Strings needed for the wireframes.
          *
          * Fortunately parsing is easy:  constructor takes the JSON string and converts it
@@ -126,14 +103,6 @@ public class ForecastFragment extends Fragment {
                 throws JSONException {
 
             // These are the names of the JSON objects that need to be extracted.
-            final String OWM_LIST = "list";
-            final String OWM_WEATHER = "weather";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_MAX = "max";
-            final String OWM_MIN = "min";
-            final String OWM_DESCRIPTION = "main";
-
-            //Moj kod
             final String LISTA_ISPISVANJE = "ispisivanje";
             final String KATEGORIJA = "kategorija";
             final String ID_SLIKE = "idSlike";
@@ -149,79 +118,47 @@ public class ForecastFragment extends Fragment {
             JSONObject ispisivanjeJson = new JSONObject(forecastJsonStr);
             JSONArray ispisivanjeArray = ispisivanjeJson.getJSONArray(LISTA_ISPISVANJE);
 
-            ArrayList<String> rezultat = new ArrayList<String>();
-            for(int j = 0; j < ispisivanjeArray.length(); j++) {
-                JSONObject ispisObject = ispisivanjeArray.getJSONObject(j);
-                JSONArray ponudaArray = ispisObject.getJSONArray(LISTA);
-                String[] resultStrs2 = new String[ponudaArray.length()];
-                    for(int k = 0; k < ponudaArray.length(); k++) {
-                        JSONObject ponudaObject = ponudaArray.getJSONObject(k);
-                        String ponuda = ponudaObject.getString("ponuda");
-                        resultStrs2[k] = ponuda;
-                        rezultat.add(resultStrs2[k]);
+            ArrayList<String> bazaList = new ArrayList<String>();
+            for(int i = 0; i < ispisivanjeArray.length(); i++) {
+                JSONObject ispisObject = ispisivanjeArray.getJSONObject(i);
+                String kategorija = ispisObject.getString("kategorija");
+                String idSlike = ispisObject.getString("idSlike");
+                String urlSlike= ispisObject.getString("urlSlike");
+                String textSlike = ispisObject.getString("textSlike");
+                JSONArray listaArray = ispisObject.getJSONArray(LISTA);
+                String[] ponudaArray = new String[listaArray.length()];
+                String[] cijenaArray = new String[listaArray.length()];
+                String[] opisArray = new String[listaArray.length()];
+                String[] urlDesnaSlikaArray = new String[listaArray.length()];
+                    for(int j = 0; j < listaArray.length(); j++) {
+                        JSONObject listaObject = listaArray.getJSONObject(j);
+                        String ponuda = listaObject.getString("ponuda");
+                        ponudaArray[j] = ponuda;
+                        String cijena = listaObject.getString("cijena");
+                        cijenaArray[j] = cijena;
+                        String opis = listaObject.getString("opis");
+                        opisArray[j] = opis;
+                        String urlDesnaSlika = listaObject.getString("urlDesnaSlika");
+                        urlDesnaSlikaArray[j] = urlDesnaSlika;
+                        bazaList.add(kategorija);
+                        bazaList.add(idSlike);
+                        bazaList.add(urlSlike);
+                        bazaList.add(textSlike);
+                        bazaList.add(ponudaArray[j]);
+                        bazaList.add(cijenaArray[j]);
+                        bazaList.add(opisArray[j]);
+                        bazaList.add(urlDesnaSlikaArray[j]);
                     }
 
             }
-            String[] resultStrs1 = new String[rezultat.size()];
-            resultStrs1 = rezultat.toArray(resultStrs1);
-/*
-            JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+            //Niz elemenata polja ponuda u JSON-u
+            String[] bazaArray = new String[bazaList.size()];
+            bazaArray = bazaList.toArray(bazaArray);
 
-            // OWM returns daily forecasts based upon the local time of the city that is being
-            // asked for, which means that we need to know the GMT offset to translate this data
-            // properly.
-
-            // Since this data is also sent in-order and the first day is always the
-            // current day, we're going to take advantage of that to get a nice
-            // normalized UTC date for all of our weather.
-
-            Time dayTime = new Time();
-            dayTime.setToNow();
-
-            // we start at the day returned by local time. Otherwise this is a mess.
-            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-
-            // now we work exclusively in UTC
-            dayTime = new Time();
-
-            String[] resultStrs = new String[weatherArray.length()];
-            for(int i = 0; i < weatherArray.length(); i++) {
-                // For now, using the format "Day, description, hi/low"
-                String day;
-                String description;
-                String highAndLow;
-
-                // Get the JSON object representing the day
-                JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-                // The date/time is returned as a long.  We need to convert that
-                // into something human-readable, since most people won't read "1400356800" as
-                // "this saturday".
-                long dateTime;
-                // Cheating to convert this to UTC time, which is what we want anyhow
-                dateTime = dayTime.setJulianDay(julianStartDay+i);
-                day = getReadableDateString(dateTime);
-
-                // description is in a child array called "weather", which is 1 element long.
-                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                description = weatherObject.getString(OWM_DESCRIPTION);
-
-                // Temperatures are in a child object called "temp".  Try not to name variables
-                // "temp" when working with temperature.  It confuses everybody.
-                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.getDouble(OWM_MAX);
-                double low = temperatureObject.getDouble(OWM_MIN);
-
-                highAndLow = formatHighLows(high, low);
-                resultStrs[i] = day + " - " + description + " - " + highAndLow;
-
-            }
-*/
-            for (String s : resultStrs1) {
+            for (String s : bazaArray) {
                 Log.i(LOG_TAG, "Ponuda: " + s);
             }
-            return resultStrs1;
+            return bazaArray;
 
         }
 
